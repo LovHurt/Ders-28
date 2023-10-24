a6a1abf6f75ba2d5fa069a73960cad0c9fb2b1f3
Alican Dağıdır <alicandagidir@hotmail.com>
2023-10-24 16:21:22 +0300

    Add project files.

---
+++ Ders 28/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders_28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] students = new string[3];
+            students[0] = "Veli";
+            students[1] = "Mehmet";
+            students[2] = "Ahmet";
+
+            string[] students2 = new[] { "Engin", "Derin", "Salih" };
+
+            string[] students3 = { "X", "Y", "Z" };
+
+            string[] students4 = new string[3]{ "A", "B", "C" };
+
+
+            foreach (var student in students) { 
+            
+                Console.WriteLine(student);
+                
+            }
+            string[,] regions = new string[5, 3]
+            {
+                {"İstanbul", "İzmit", "Balıkesir" },
+                {"Ankara", "Konya", "Kırıkkale" },
+                {"Antalya", "Adana", "Mersin" },
+                {"İzmir", "Muğla", "Aydın" },
+                {"Rize", "Trabzon", "Manisa" }
+            };
+
+            for (int i = 0; i <=regions.GetUpperBound(0) i++)
+            {
+                for (int j = 0; j < regions.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine(regions[i, j]);
+                }
+                Console.WriteLine("*************");
+            }
+
+            Console.ReadLine();
+
+
+
+        }
+
+      
+    }
+}
